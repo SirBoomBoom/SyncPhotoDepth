607ef7a0e353f830bf28d679f60c5d65d1ec580b
SirBoomBoom <braun.junk.mail@gmail.com>
2023-04-16 03:37:21 -0700

    Added argument parser Added support for LatLong locations Added support for Camera & Dive Computer offsets Added FREEDOM Suppression option for those who have strong feelings about standards Optimized code some to reduce load during picture loop for tags that are the same across all photos

--- SyncPhotoDepth.py
+++ SyncPhotoDepth.py
@@ -4,25 +4,112 @@ import sys
 import datetime
 import calendar
 import pyexiv2
+import argparse
+import lat_lon_parser
 
-# Remove 1st argument from the list of command line arguments
-argumentList = sys.argv[1:]
-#Parse arguments
-fFile = argumentList[0]
+parser = argparse.ArgumentParser()
+parser.add_argument("fitFile", type=str, help="The FIT File to use when sycning depth and temperature to photos")
+parser.add_argument("-z", "--timezone", type=str, help="The timezone the photos were taken in, using the format \" -08:00\" to represent something like PST. " + 
+                    "NOTE: If timezone offset starts with a leading '-' it must be quoted and have a leading space, as otherwise Python insists on treating it as an optional argument")
+parser.add_argument("-o", "--offset", type=int, default = 0, help="Optional picture offset, in seconds, from the FIT file. This is used to offset any clock drift between camera and dive computer" + 
+                    "eg. If the Camera clock was 17 seconds behind the Dive Computer Clock you'd pass '-o 17', for an hour and 17 seconds early you'd pass -3617")
+parser.add_argument("-l", "--location", type=str, help="Optional human friendly location of the photos to add to the metadata, eg. 'Mukilteo, WA'")
+parser.add_argument("-c", "--coords", type=str, help="Optional GPS coordinates to add to the photo. Should be in the form \"##.### -##.###\" I honestly have no idea why this one doesn't always need the leading space when negative.")
+parser.add_argument("-F", "--FREEDOM", type=str, default="TRUE", help="Standards are for other people. Why use Meters and C° when Feet and F° exist? Setting this to true ignores the " + 
+                    "standard in favor of using feet and Fahrenheit instead. As the author is from MERICA, this is the default behaviour. Expects some form of False or 'F', everything else evaluates True")
+parser.add_argument("-a", "--author", type=str, help="Optional string for Author/Copywrite")
+parser.add_argument("-v", "--verbose", action="store_true", help="Flood the console with Print statements")
+
+args = parser.parse_args()
+#Create the dictionary for data we'll be adding to photos in advance, because half of it will be identical for all photos so there's no sense updating it in our photo loop
+addData = {}
+
+#Get the FIT File name
+fFile = args.fitFile
+print("FIT File: " + fFile)
 
 #Set timezone for photos cause they're stupid
-picTimezone = argumentList[1]
+if args.timezone is not None:
+    picTimezone = args.timezone.lstrip()
+    print("TimeZone: " + picTimezone)
+else:
+    picTimezone = None
+    print("TimeZone: None")
 
-#Optional human friendly location to set for photos. Should probably make this a '-l/-loc' type option...
-picLoc = argumentList[2]
+#Set the picture offset, if present
+picOffset = args.offset
+if picOffset is not None:
+    print("Picture offset (in seconds): " + str(picOffset))
+else:
+    print("Picture offset (in seconds): None")
 
-##Converts Celsius to Fahrenheit
+#Optional human friendly location to set for photos.
+picLoc = args.location
+if picLoc is not None:
+    print("Picture Location: " + picLoc)
+    #Set the "name/location" of the dive. Overloading UniqueID because ReelName isn't displayed by default... <sigh>    
+    addData.update({'Exif.Photo.ImageUniqueID': picLoc, 'Exif.Image.ReelName' : picLoc})
+else:
+    print("Picture Location: None")
+
+#Converts a decimal coordinate into an ugly Degree Minute Second as fractions abomination. hem wants either ["N", "S"] for latitude or ["E", "W"] for longitude
+def convertCoord(coord, hem):
+    #Because of course EXIF doesn't support signed coordinates, we need to strip negative signs and track them by hemisphere >.<
+    if coord.startswith('-'):
+        coord = coord[1:]
+        ref = hem[1]
+    else:
+        ref = hem[0]        
+        
+    #Convert our lovely decimal coordinates into Degrees, Minutes and Seconds
+    deg = lat_lon_parser.to_deg_min_sec(lat_lon_parser.parse(coord))
+    #Now that we have it as degrees and minutes, we need to convert to fractions because EXIF >.<
+    deg = (str(round(deg[0]))+"/1", str(round(deg[1]))+"/1", str(round(deg[2]*100))+"/100")
+    return [deg, ref]
+
+#Optional decimal coordinates to set for photos. This will be converted to degrees minutes if not none
+if args.coords is not None:
+    coords = str.split(args.coords)
+    lat = convertCoord(coords[0], ["N", "S"])
+    long = convertCoord(coords[1], ["E", "W"])
+    picCoords = [lat, long]
+    print("GPS Coords: " + str(picCoords))
+    #Because of course things cannot be obvous or documented correctly >.< Lat and Long must be passed as a single string of fractions seperated by spaces (NO COMMAS)
+    addData.update({"Exif.GPSInfo.GPSLatitude": str(picCoords[0][0][0])+" "+str(picCoords[0][0][1])+" "+str(picCoords[0][0][2]), "Exif.GPSInfo.GPSLatitudeRef": picCoords[0][1],
+                    "Exif.GPSInfo.GPSLongitude": str(picCoords[1][0][0])+" "+str(picCoords[1][0][1])+" "+str(picCoords[1][0][2]), "Exif.GPSInfo.GPSLongitudeRef": picCoords[1][1]})
+else:
+    picCoords = args.coords
+    print("GPS Coords: None")
+
+#Optional flag to ignore the EXIF standards in favor of using Feet and Fahrenheit where Meters and Celsius normall go
+if 'FALSE'.startswith(args.FREEDOM.upper()):
+    ignoreStandards = False
+else:
+    ignoreStandards = True
+print("FREEDOM: " + str(ignoreStandards))
+
+#Get the Author name/Copyright
+author = args.author
+if author is not None:
+    print("Author/Copyright: " + author)
+    #Author is apparently a Windows only thing, so setting Copyright as well
+    addData.update({'Exif.Image.Copyright': author, 'Exif.Image.XPAuthor': author}) 
+else:
+    print("Author/Copyright: None")
+
+##Converts Celsius to Fahrenheit if FREEDOM is set to true
 def cToF(temp):
-    return round((temp * 9 / 5 + 32))
+    if ignoreStandards:
+        return round((temp * 9 / 5 + 32))
+    else:
+        return temp
 
-#Converts millimeters to feet
+#Converts millimeters to feet or meters depending on FREEDOM
 def mmToFeet(mm):
-    return round(mm/304.8)
+    if ignoreStandards:
+        return round(mm/304.8)
+    else:
+        return round(mm/1000,1)
 
 #Sorted list of dive data points
 dataPoints = []
@@ -42,7 +129,7 @@ for pic in sorted(os.listdir(), key=os.path.getmtime):
         #Close the image because the library tells us to
         exiv_image.close()
         #Convert to Epoch so we can stop dealing with the pain that is DateTime objects
-        pictures.append((pic, t.timestamp()))
+        pictures.append((pic, (t.timestamp() + picOffset)))
     except:
         continue
 #Sort the pictures by their metadata created date, just in case the OS was sorting by a different timestamp
@@ -91,18 +178,17 @@ for pic in pictures:
         offset = dataPoints[k][0] - pic[1]
         #Calculate the depth assuming a linear change between the two points, then convert to feet. Convert to a fraction because apparently that is required to work
         depth = str(mmToFeet(nextDepth - ((nextDepth - prevDepth)/(timeRange) * (offset)))) + "/1"
-        print("Found depth: " + str(depth))
+        if args.verbose:
+            print("Found depth: " + str(depth))
         
         # Create set of metadata we wish to update. This will overwrite its original value, or add it if it doesn't exist
-        addData = {'Exif.Image.Copyright': "Don Mitchell",
-                   # #WaterDepth (Technically supposed to be in meters, buuuutt.... 'MERICA!)
-                   'Exif.Photo.WaterDepth': depth, 
+        addData.update({'Exif.Photo.WaterDepth': depth, #WaterDepth (Technically supposed to be in meters, buuuutt.... 'MERICA!)
                    #Temperature also supposed to be in Celsius, but that's just silly. Almost as silly as the fact that it must be saved as a fraction, as opposed to a float or something sane.
                    'Exif.Photo.Temperature': str(dataPoints[k][2]) + "/1", 
                    #Set the Altitude, because WaterDepth doesn't actually show up >.< Ref of 1 indicates Altitude is below sea level (sadly it is the absolute value so will display as positive number)
-                   'Exif.GPSInfo.GPSAltitudeRef': 1, 'Exif.GPSInfo.GPSAltitude': depth,
-                   #Set the "name/location" of the dive. Overloading UniqueID because ReelName isn't displayed by default... <sigh>
-                   'Exif.Photo.ImageUniqueID': picLoc, 'Exif.Image.ReelName' : picLoc}
+                   'Exif.GPSInfo.GPSAltitudeRef': 1, 'Exif.GPSInfo.GPSAltitude': depth})
+        if args.verbose:
+            print(addData)
 
         #Fetch the metadata object we wish to edit
         image = pyexiv2.Image(pic[0])
